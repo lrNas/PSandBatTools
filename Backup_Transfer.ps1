368ca6cac1181bcceb1c2aaed3b447d9d2f4ff79
lrNas <lr.nas1894@gmail.com>
2022-09-29 18:07:05 -0300

    Solicitar dados de credenciais

--- Backup_Transfer.ps1
+++ Backup_Transfer.ps1
@@ -1,14 +1,12 @@
 ﻿
-
-#Recuperar hostname da nova máquina do cliente em algum lugar e armazenar nesse target
-
-$target = Read-host 'Entre o hostname da máquina de destino:'
-#Detectar se é C ou se é E
-$pathOrigem = 'C:\Backup'
+$pathOrigem = Read-host 'Entre o caminho da pasta a ser copiada:'
+$target = Read-host 'Entre o hostname ou ip da máquina de destino:'
 $pathDestino = '\\'+$target+'\c$'
-
+Write-Host "Credenciais de acesso à máquina:"
+$user = Read-Host "Usuário"
+$pass = Read-Host "Senha"
 $net = new-object -ComObject WScript.Network
-$net.MapNetworkDrive("u:", $pathDestino, $false, 'usuario', 'senha')
+$net.MapNetworkDrive("u:", $pathDestino, $false, $user, $pass)
 
 #Iterar arquivos em vez de copiar recursivamente, pra em caso de falha começar do ultimo arquivo recebido.
 Copy-Item $pathOrigem -Destination $pathDestino -Force -Recurse
